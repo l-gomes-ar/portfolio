17d0417cdc4c7c46880d7b91d2893473b0f0d6d2
l-gomes-ar <lgaraujo021@gmail.com>
2024-08-06 15:56:10 -0300

    Updated footer

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -8,24 +8,24 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 export default function Footer() {
     return (
         <>
-            <div className={`${paragraph.className} text-center sm:flex sm:justify-center sm:gap-10 lg:gap-[9rem] mt-10 p-6 border-t-2 border-black text-lg`}>
-                <p className={`${signature.className} text-2xl`}>Lucas G. Araujo</p>
-                <div>
-                    <p className="font-bold">About Me</p>
+            <div className={`${paragraph.className} text-center sm:flex sm:justify-center sm:items-baseline sm:gap-10 lg:gap-[9rem] mt-10 p-6 border-t-2 border-black text-lg`}>
+                <p className={`${signature.className} text-2xl`}>Lucas Gomes Araujo</p>
+                <div className="mt-4 sm:mt-0 sm:text-start">
+                    <p className="font-bold text-xl">About Me</p>
                     <ul className="text-gray-600 font-thin">
                         <li><Link href="#">Summary</Link></li>
                         <li><Link href="#">Resume</Link></li>
                     </ul>    
                 </div>
-                <div>
-                    <p className="font-bold">Projects</p>
+                <div className="mt-4 sm:mt-0 sm:text-start">
+                    <p className="font-bold text-xl">Projects</p>
                     <ul className="text-gray-600 font-thin">
                         <li><Link href='#'>Frontend</Link></li>
                         <li><Link href='#'>Backend</Link></li>
                         <li><Link href='#'>Full Stack</Link></li>
                     </ul>
                 </div>
-                <div>
+                <div className="mt-4 sm:mt-0">
                     <Link href={'mailto:lgaraujo022@gmail.com'}>
                         <IconButton aria-label="email">
                             <EmailIcon className="text-black" fontSize="large" />
