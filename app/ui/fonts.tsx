0479f57bbf93eba39c82ba16127828f906afb3fc
l-gomes-ar <lgaraujo021@gmail.com>
2024-08-20 02:32:01 +0100

    Updated paragraph font from ubuntu to ubuntu mono

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,8 +1,8 @@
-import { Ubuntu, Merriweather, Dancing_Script } from 'next/font/google';
+import { Merriweather, Dancing_Script, Ubuntu_Mono } from 'next/font/google';
 
 export const heading = Merriweather({ weight: "700", subsets: ['latin'] });
 export const subheading = Merriweather({ weight: "300", subsets: ['latin'] });
 
-export const paragraph = Ubuntu({ weight: ['300', '400', '700'], subsets: ['latin'] });
+export const paragraph = Ubuntu_Mono({ weight: ['400', '700'], subsets: ['latin'] });
 
 export const signature = Dancing_Script({ weight: '700', subsets: ['latin'] });
